b1b2db8ad89e9c6deefa8e0ce5aaf7f1ea5eb02a
Patryk Nadrowski <nadro-linux@wp.pl>
2019-11-07 22:45:41 +0100

    - bumped libcurl version

--- depend/curl/build.py
+++ depend/curl/build.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-libraryVersion = '7.66.0'
+libraryVersion = '7.67.0'
 libraryName = 'curl-' + libraryVersion
 
 buildCommonFile = os.path.join('..', '..', 'script', 'build_common.py')
